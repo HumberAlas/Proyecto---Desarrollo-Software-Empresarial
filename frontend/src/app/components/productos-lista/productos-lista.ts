9a26dbc9c8401cc5ab4e8ada4caade936db76ab2
zFlixT <pt0109032023@unab.edu.sv>
2026-04-13 23:11:22 -0600

    feat: Creación de la lista de productos

--- frontend/src/app/components/productos-lista/productos-lista.ts
+++ frontend/src/app/components/productos-lista/productos-lista.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductoService } from "../../../services/producto.service";
+import { Producto } from '../../../models/producto.model';
 
 @Component({
-  selector: 'app-productos-lista',
-  imports: [],
-  templateUrl: './productos-lista.html',
-  styleUrl: './productos-lista.css',
+selector: 'app-productos-lista',
+standalone: true,
+imports: [CommonModule],
+templateUrl: './productos-lista.component.html',
+styleUrl: './productos-lista.component.css'
 })
-export class ProductosLista {
+export class ProductosListaComponent {
+productos: Producto[] = [];
 
+constructor(private productoService: ProductoService) {
+this.productos = this.productoService.obtenerProductos();
+}
 }
