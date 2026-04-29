f7afae6405dcbb7754f545b486be6c2068b2752a
Kenia17 <126429254+Kenia17@users.noreply.github.com>
2026-04-28 20:52:49 -0600

    feat: crear modelo y endpoints get de usuarios

---
+++ backend/src/models/usuario.model.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const usuarioSchema = new mongoose.Schema(
+  {
+    nombre: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    correo: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true
+    },
+    rol: {
+      type: String,
+      required: true,
+      enum: ["Product Owner", "Scrum Master", "Developer", "Administrador"]
+    }
+  },
+  {
+    timestamps: true
+  }
+);
